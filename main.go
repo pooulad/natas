33616cd6c0919913bfee65b875fd65559926fa30
pooulad <poooooladi@gmail.com>
2023-11-05 21:04:20 +0330

    create all directories

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func checkError(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	count := 34
+	dir := "natas"
+	fileName := "README"
+	extention := "md"
+
+	for i := 1; i < count+1; i++ {
+		dirName := fmt.Sprint(dir, i)
+		os.Mkdir(dirName, 0777)
+
+		f, err := os.Create(fmt.Sprintf("./%v/%v.%v", dirName, fileName, extention))
+		checkError(err)
+
+		_, err = f.Write([]byte(fmt.Sprintf("# %v", dirName)))
+		checkError(err)
+
+		fmt.Println("new file created")
+		defer f.Close()
+	}
+}
